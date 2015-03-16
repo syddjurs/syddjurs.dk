d2899518c4d35aef3b4803099338b638f579f804
Thomas Thune Hansen <tth@bellcom.dk>
2015-03-16 08:37:28 +0100

    Started doing the new cp-service page

---
+++ public_html/profiles/os2web/modules/os2web_cp_service/os2web-cp-service-page.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Template file for CP service overview page.
+ */
+?>
+
+
+<?php print render($datepicker) ?>
+
+<p>Der vises dokumenter fra <?php print $date_string; ?>.</p>
+
+<?php print render($document_table) ?>
