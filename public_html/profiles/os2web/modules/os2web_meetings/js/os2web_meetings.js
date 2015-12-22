d7c9a5bcaa659422e5b62aef00b20e6887a7d60d
Mathias Larsen <tripox@tripox.dk>
2015-12-22 13:00:17 +0100

    Fix 'accordions' on dagsorden mobile

--- public_html/profiles/os2web/modules/os2web_meetings/js/os2web_meetings.js
+++ public_html/profiles/os2web/modules/os2web_meetings/js/os2web_meetings.js
@@ -13,6 +13,17 @@ Drupal.behaviors.os2web_meetings = {
       var top = $(this).nextAll('.agenda-item').offset().top;
       window.scrollTo(0, top -50);
     });
+    $(window).on('load ready', function() {
+      $('a.agenda-bullet-list').click(function(e) {
+        e.preventDefault();
+        $(this).nextAll('.agenda-item').toggle();
+        $(this).toggleClass('open');
+        $('a.agenda-bullet-list').not(this).nextAll('.agenda-item').hide();
+        $('a.agenda-bullet-list').not(this).removeClass('open');
+        var top = $(this).nextAll('.agenda-item').offset().top;
+        window.scrollTo(0, top -50);
+      });
+    });
   }
 };
 }(jQuery));
