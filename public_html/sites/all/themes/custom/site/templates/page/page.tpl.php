db8ebeeabcdd0739bccd82498b91eceb1ae73cd8
Morten Nissen <mn@bellcom.dk>
2016-12-19 10:52:59 +0100

    Update site theme layout

--- public_html/sites/all/themes/custom/site/templates/page/page.tpl.php
+++ public_html/sites/all/themes/custom/site/templates/page/page.tpl.php
@@ -20,11 +20,11 @@
             </div>
             <!-- End - logo -->
 
-            <?php if (isset($sidebar_tertiary)): ?>
-                <!-- Begin - navigation -->
-                <?php print render($sidebar_tertiary); ?>
-                <!-- End - navigation -->
-            <?php endif; ?>
+          <?php if (isset($sidebar_tertiary)): ?>
+              <!-- Begin - navigation -->
+            <?php print render($sidebar_tertiary); ?>
+              <!-- End - navigation -->
+          <?php endif; ?>
 
         </div>
         <!-- End - inner wrapper -->
@@ -35,13 +35,13 @@
     <!-- Begin - inner wrapper -->
     <div class="inner-wrapper" role="document">
 
-        <?php if (isset($emergency_grant)): ?>
-            <!-- Begin - emergency grant -->
-            <div class="os2-emergency-grant">
-                <?php print render($emergency_grant); ?>
-            </div>
-            <!-- End - emergency grant -->
-        <?php endif; ?>
+      <?php if (isset($emergency_grant)): ?>
+          <!-- Begin - emergency grant -->
+          <div class="os2-emergency-grant">
+            <?php print render($emergency_grant); ?>
+          </div>
+          <!-- End - emergency grant -->
+      <?php endif; ?>
 
         <!-- Begin - simple navigation -->
         <nav class="simple-navigation">
@@ -60,8 +60,10 @@
             <!-- End - button list -->
 
             <!-- Begin - logo -->
-            <a href="<?php echo $front_page; ?>" class="simple-navigation-logo-link">
-                <img src="<?php echo $path_img . '/logo-simple-navigation.png'; ?>" class="simple-navigation-logo-image"
+            <a href="<?php echo $front_page; ?>"
+               class="simple-navigation-logo-link">
+                <img src="<?php echo $path_img . '/logo-simple-navigation.png'; ?>"
+                     class="simple-navigation-logo-image"
                      alt="<?php echo t('fredericia.dk logo'); ?>"/>
             </a>
             <!-- End - logo -->
@@ -72,102 +74,119 @@
         <!-- Begin - content -->
         <div class="content">
 
-            <!-- Begin - main navigation -->
-            <nav class="main-navigation-wrapper">
-                <section class="main-navigation-bar">
-                    <div class="container">
-                        <div class="row">
-
-                            <!-- Begin - content -->
-                            <div class="col-md-4">
-                                <a href="<?php echo $front_page; ?>" class="main-navigation-logo-link">
-                                    <img src="<?php echo $path_img; ?>/logo-main-navigation.png" alt=""
-                                         class="main-navigation-logo-image">
-                                </a>
-                            </div>
-                            <!-- End - content -->
-
-                            <!-- Begin - content -->
-                            <?php if (isset($main_navigation_secondary)): ?>
-                                <div class="col-md-8 main-navigation-right">
-
-                                    <!-- Begin - navigation -->
-                                    <?php print render($main_navigation_secondary); ?>
-                                    <!-- End - navigation -->
-
-                                </div>
-                            <?php endif; ?>
-                            <!-- End - content -->
-
-                        </div>
-                    </div>
-                </section>
-            </nav>
-            <!-- End - main navigation -->
-
             <!-- Begin - page header -->
             <div class="os2-page-header">
                 <div class="container">
 
-                    <!-- Begin - search -->
                     <div class="row">
-                        <div class="os2-page-header-search-form">
-                            <?php print render($page_header_search); ?>
-                        </div>
-                    </div>
-                    <!-- End - search -->
 
-                    <!-- Begin - menu -->
-                    <div class="row">
-                        <div class="os2-page-header-tabbed-menu">
-                            <?php print render($tabbed_navigation); ?>
-                        </div>
+                        <!-- Begin - user first -->
+                      <?php if (!empty($page['user_first'])): ?>
+                        <?php print render($page['user_first']); ?>
+                      <?php endif; ?>
+                        <!-- End - user first -->
+
+                        <!-- Begin - user second -->
+                      <?php if (!empty($page['user_second'])): ?>
+                        <?php print render($page['user_second']); ?>
+                      <?php endif; ?>
+                        <!-- End - user second -->
+
+                        <!-- Begin - preface first -->
+                      <?php if (!empty($page['preface_first'])): ?>
+                        <?php print render($page['preface_first']); ?>
+                      <?php endif; ?>
+                        <!-- End - preface first -->
+
                     </div>
-                    <!-- End - menu -->
+                    <div class="row">
+
+                        <!-- Begin - branding -->
+                      <?php if (!empty($page['branding'])): ?>
+                        <?php print render($page['branding']); ?>
+                      <?php endif; ?>
+                        <!-- End - branding -->
+
+                        <!-- Begin - header second -->
+                      <?php if (!empty($page['header_second'])): ?>
+                        <?php print render($page['header_second']); ?>
+                      <?php endif; ?>
+                        <!-- End - header second -->
+
+                    </div>
+                    <div class="row">
+
+                        <!-- Begin - menu -->
+                      <?php if (!empty($page['menu'])): ?>
+                        <?php print render($page['menu']); ?>
+                      <?php endif; ?>
+                        <!-- End - menu -->
+
+                        <!-- Begin - search -->
+                      <?php if (!empty($page['search'])): ?>
+                        <?php print render($page['search']); ?>
+                      <?php endif; ?>
+                        <!-- End - search -->
+
+                    </div>
+
+                    <!-- Begin - header first -->
+                  <?php if (!empty($page['header_first'])): ?>
+                    <?php print render($page['header_first']); ?>
+                  <?php endif; ?>
+                    <!-- End - header first -->
+
+                    <!-- Begin - preface second -->
+                  <?php if (!empty($page['preface_second'])): ?>
+                    <?php print render($page['preface_second']); ?>
+                  <?php endif; ?>
+                    <!-- End - preface second -->
 
                 </div>
             </div>
             <!-- End - page header -->
 
-            <?php if (!empty($breadcrumb)): ?>
-                <!-- Begin - breadcrumb -->
-                <section class="os2-breadcrumb-container">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-xs-12">
-                                <?php print $breadcrumb; ?>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <!-- End - breadcrumb -->
-            <?php endif; ?>
+          <?php if (!empty($breadcrumb)): ?>
+              <!-- Begin - breadcrumb -->
+              <section class="os2-breadcrumb-container">
+                  <div class="container">
+                      <div class="row">
+                          <div class="col-xs-12">
+                            <?php print $breadcrumb; ?>
+                          </div>
+                      </div>
+                  </div>
+              </section>
+              <!-- End - breadcrumb -->
+          <?php endif; ?>
 
             <div class="container">
 
-                <?php if (!empty($page['help'])): ?>
-                    <?php print render($page['help']); ?>
-                <?php endif; ?>
+              <?php if (!empty($page['help'])): ?>
+                <?php print render($page['help']); ?>
+              <?php endif; ?>
 
-                <?php if (!empty($action_links)): ?>
-                    <ul class="action-links"><?php print render($action_links); ?></ul>
-                <?php endif; ?>
+              <?php if (!empty($action_links)): ?>
+                  <ul class="action-links"><?php print render(
+                      $action_links
+                    ); ?></ul>
+              <?php endif; ?>
 
-                <?php if (!empty($tabs_primary)): ?>
-                    <!-- Begin - tabs primary -->
-                    <div class="os2-tabs-container os2-tabs-variant-default">
-                        <?php print render($tabs_primary); ?>
-                    </div>
-                    <!-- End - tabs primary -->
-                <?php endif; ?>
+              <?php if (!empty($tabs_primary)): ?>
+                  <!-- Begin - tabs primary -->
+                  <div class="os2-tabs-container os2-tabs-variant-default">
+                    <?php print render($tabs_primary); ?>
+                  </div>
+                  <!-- End - tabs primary -->
+              <?php endif; ?>
 
-                <?php if (!empty($tabs_secondary)): ?>
-                    <!-- Begin - tabs secondary -->
-                    <div class="os2-tabs-container os2-tabs-variant-tertiary">
-                        <?php print render($tabs_secondary); ?>
-                    </div>
-                    <!-- End - tabs secondary -->
-                <?php endif; ?>
+              <?php if (!empty($tabs_secondary)): ?>
+                  <!-- Begin - tabs secondary -->
+                  <div class="os2-tabs-container os2-tabs-variant-tertiary">
+                    <?php print render($tabs_secondary); ?>
+                  </div>
+                  <!-- End - tabs secondary -->
+              <?php endif; ?>
 
             </div>
 
@@ -175,44 +194,97 @@
             <div class="message">
               <?php print $messages; ?>
             </div>
-            <?php if (panels_get_current_page_display()): ?>
-                <?php if ($wrap_panels_layout): ?>
-                    <div class="container">
-                        <?php print render($page['content']); ?>
-                    </div>
-                <?php else: ?>
+          <?php if (panels_get_current_page_display()): ?>
+            <?php if ($wrap_panels_layout): ?>
+                  <div class="container">
                     <?php print render($page['content']); ?>
-                <?php endif; ?>
+                  </div>
             <?php else: ?>
-                <div class="container">
-                    <div class="os2-box">
-                        <div class="os2-box-body">
-                            <?php print render($page['content']); ?>
-                        </div>
-                    </div>
-                </div>
+              <?php print render($page['content']); ?>
             <?php endif; ?>
+          <?php else: ?>
+              <div class="container">
+
+                  <!-- Begin - sidebar first -->
+                <?php if (!empty($page['sidebar_first'])): ?>
+                  <?php print render($page['sidebar_first']); ?>
+                <?php endif; ?>
+                  <!-- End - sidebar first -->
+
+                  <!-- Begin - content -->
+                <?php print render($page['content']); ?>
+                  <!-- End - content -->
+
+                  <!-- Begin - sidebar second -->
+                <?php if (!empty($page['sidebar_second'])): ?>
+                  <?php print render($page['sidebar_second']); ?>
+                <?php endif; ?>
+                  <!-- End - sidebar second -->
+
+                  <!--                  <div class="os2-box">-->
+                  <!--                      <div class="os2-box-body">-->
+                  <!--                        --><?php //print render($page['content']); ?>
+                  <!--                      </div>-->
+                  <!--                  </div>-->
+              </div>
+          <?php endif; ?>
 
         </div>
         <!-- End - content -->
 
-        <!-- Begin - footer -->
-        <?php if (!empty($page['footer'])): ?>
-            <footer class="footer">
-                <div class="container">
-                    <div class="footer-content">
-                        <div class="row">
+        <!-- Begin - footer first -->
+      <?php if (!empty($page['footer_first'])): ?>
+          <footer class="footer">
+              <div class="container">
+                  <div class="footer-content">
+                      <div class="row">
 
-                            <div class="col-xs-12">
-                                <?php print render($page['footer']); ?>
-                            </div>
+                          <div class="col-xs-12">
+                            <?php print render($page['footer_first']); ?>
+                          </div>
 
-                        </div>
-                    </div>
-                </div>
-            </footer>
-        <?php endif; ?>
-        <!-- End - footer -->
+                      </div>
+                  </div>
+              </div>
+          </footer>
+      <?php endif; ?>
+        <!-- End - footer first -->
+
+        <!-- Begin - footer second -->
+      <?php if (!empty($page['footer_second'])): ?>
+          <footer class="footer">
+              <div class="container">
+                  <div class="footer-content">
+                      <div class="row">
+
+                          <div class="col-xs-12">
+                            <?php print render($page['footer_second']); ?>
+                          </div>
+
+                      </div>
+                  </div>
+              </div>
+          </footer>
+      <?php endif; ?>
+        <!-- End - footer second -->
+
+        <!-- Begin - footer third -->
+      <?php if (!empty($page['footer_third'])): ?>
+          <footer class="footer">
+              <div class="container">
+                  <div class="footer-content">
+                      <div class="row">
+
+                          <div class="col-xs-12">
+                            <?php print render($page['footer_third']); ?>
+                          </div>
+
+                      </div>
+                  </div>
+              </div>
+          </footer>
+      <?php endif; ?>
+        <!-- End - footer third -->
 
     </div>
     <!-- End - inner wrapper -->
