3eedc24b4e23238df95abb4556fb0179ddccb5a4
Franz Skaaning <fs@bellcom.dk>
2016-12-21 14:18:37 +0100

    Styles til site

--- public_html/sites/all/themes/custom/site/templates/from-old-design/--region--menu.tpl.php
+++ public_html/sites/all/themes/custom/site/templates/from-old-design/--region--menu.tpl.php
@@ -6,12 +6,12 @@
 ?>
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
-    <?php if ($main_menu || $secondary_menu): ?>
-    <nav class="navigation nav">
+    <?php // if ($main_menu || $secondary_menu): ?>
+    <nav class="navigation nav nav-pills">
       <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix', 'main-menu')), )); ?>
       <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix', 'secondary-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); ?>
     </nav>
-    <?php endif; ?>
+    <?php // endif; ?>
     <?php print $content; ?>
   </div>
 </div>
