8d41711de6286eba513437cc03adba1530e1d547
Franz Skaaning <fs@bellcom.dk>
2017-02-28 21:18:54 +0100

    Styles til editor

--- public_html/sites/all/themes/custom/site/ckeditor.styles.js
+++ public_html/sites/all/themes/custom/site/ckeditor.styles.js
@@ -15,14 +15,22 @@ if(typeof(CKEDITOR) !== 'undefined') {
         /* Bootstrap Styles */
 
         /* Typography */
-        { name : 'Overskift (H2)'        , element : 'h2', attributes: { 'class': 'h2' } },
-        { name : 'Afsnitsoverskift (H3)'        , element : 'h3', attributes: { 'class': 'h3' } },
-        { name : 'Overskrift 4'        , element : 'h4', attributes: { 'class': 'h4' } },
-        { name : 'Overskrift 5'        , element : 'h5', attributes: { 'class': 'h5' } },
-        { name : 'Overskrift 6'        , element : 'h6', attributes: { 'class': 'h6' } },
+        { name : 'Overskift (H2)' , element : 'h2', attributes: { 'class': 'h2' } },
+        { name : 'Afsnitsoverskift (H3)' , element : 'h3', attributes: { 'class': 'h3' } },
+        { name : 'Afsnitsoverskift Primary (H3)'        , element : 'h3', attributes: { 'class': 'h3 text-primary' } },
+        { name : 'Overskrift 4' , element : 'h4', attributes: { 'class': 'h4' } },
+        { name : 'Overskrift 5' , element : 'h5', attributes: { 'class': 'h5' } },
+        { name : 'Overskrift 6' , element : 'h6', attributes: { 'class': 'h6' } },
 
-        { name : 'Indledning'     , element : 'p', attributes: { 'class': 'lead' } },
-        { name : 'Afsnit'     , element : 'p', attributes: { } },
+        { name : 'Indledning (Lead)' , element : 'p', attributes: { 'class': 'lead' } },
+        { name : 'Afsnit', element : 'p', attributes: { 'class': 'normal' } },
+        { name : 'Afsnit - info'     , element : 'p', attributes: { 'class' : 'bg-info bg-pad10' } },
+        { name : 'Afsnit - fremhævet'     , element : 'p', attributes: { 'class' : 'well ' } },
+        { name : 'Afsnit - fremhævet stor'     , element : 'p', attributes: { 'class' : 'well well-xl  ' } },
+        { name : 'Afsnit - fremhævet lille'     , element : 'p', attributes: { 'class' : 'well well-sm  ' } },
+        { name : 'Afsnit - grøn'     , element : 'p', attributes: { 'class' : 'bg-success bg-pad10' } },
+        { name : 'Afsnit - rød'     , element : 'p', attributes: { 'class' : 'bg-danger bg-pad10' } },
+        { name : 'Afsnit - gul'     , element : 'p', attributes: { 'class' : 'bg-warning bg-pad10' } },
 
         {
             name : 'Liste',
@@ -40,6 +48,68 @@ if(typeof(CKEDITOR) !== 'undefined') {
                 'class' : 'list-inline'
             }
         },
+        {
+            name : 'Liste - grupperet',
+            element : 'ul',
+            attributes :
+            {
+                'class' : 'list-group'
+            }
+        },
+
+        {
+            name : 'Listeelement - normal',
+            element : 'li',
+            attributes :
+            {
+                'class' : ' '
+            }
+        },
+        {
+            name : 'Listeelement - grupperet',
+            element : 'li',
+            attributes :
+            {
+                'class' : 'list-group-item'
+            }
+        },
+        {
+            name : 'Listeelement - grupperet - info',
+            element : 'li',
+            attributes :
+            {
+                'class' : 'list-group-item list-group-item-info'
+            }
+        },
+        {
+            name : 'Listeelement - grupperet - rød',
+            element : 'li',
+            attributes :
+            {
+                'class' : 'list-group-item list-group-item-danger'
+            }
+        },
+        {
+            name : 'Listeelement - grupperet - gul',
+            element : 'li',
+            attributes :
+            {
+                'class' : 'list-group-item list-group-item-warning'
+            }
+        },
+        {
+            name : 'Listeelement - grupperet - grøn',
+            element : 'li',
+            attributes :
+            {
+                'class' : 'list-group-item list-group-item-success'
+            }
+        },
+
+
+
+
+
         {
             name : 'Tabel',
             element : 'table',
