15ee51ab6d25c50a1652752cbda1a6e5bc1e73c1
Franz Skaaning <fs@bellcom.dk>
2017-04-03 13:37:47 +0200

    Styles til editor

--- public_html/sites/all/themes/custom/site/ckeditor.config.js
+++ public_html/sites/all/themes/custom/site/ckeditor.config.js
@@ -4,35 +4,115 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
 /*
- WARNING: clear browser's cache after you modify this file.
- If you don't do this, you may notice that browser is ignoring all your changes.
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
  */
-CKEDITOR.editorConfig = function(config) {
-  // [ Left, Center, Right, Justified ]
-  config.justifyClasses = [ 'text-left', 'text-center', 'text-right', 'text-justify' ];
-  config.wsc_lang = "da_DK";
-  config.scayt_defLan = 'da_DK';
-  config.allowedContent = true;
-};
+if(typeof(CKEDITOR) !== 'undefined') {
+	// config.stylesSet.push({ name : 'Nice Table', element : 'table', wrap:false, attributes : { 'class' : 'mt-nice-table', 'title' :'Nice Table' } });
+    CKEDITOR.addStylesSet( 'drupal',
+    [
 
-// Remove all unneassecary inputs on table and image dialogs.
-CKEDITOR.on('dialogDefinition', function( ev ) {
-  var dialogName = ev.data.name;
-  var dialogDefinition = ev.data.definition;
-  var infoTab = dialogDefinition.getContents('info');
+        /* Bootstrap Styles */
 
-  if(dialogName === 'table' || dialogName == 'tableProperties' ) {
+        /* Typography */
+        { name : 'Overskift (H2)'        , element : 'h2', attributes: { 'class': 'h2' } },
+        { name : 'Afsnitsoverskift (H3)'        , element : 'h3', attributes: { 'class': 'h3' } },
+        { name : 'Overskrift 4'        , element : 'h4', attributes: { 'class': 'h4' } },
+        { name : 'Overskrift 5'        , element : 'h5', attributes: { 'class': 'h5' } },
+        { name : 'Overskrift 6'        , element : 'h6', attributes: { 'class': 'h6' } },
 
-    //remove fields
-    var cellSpacing = infoTab.remove('txtCellSpace');
-    var cellPadding = infoTab.remove('txtCellPad');
-    var border = infoTab.remove('txtBorder');
-    var width = infoTab.remove('txtWidth');
-    var height = infoTab.remove('txtHeight');
-    var align = infoTab.remove('cmbAlign');
-  }
+        { name : 'Indledning'     , element : 'p', attributes: { 'class': 'lead' } },
+        { name : 'Afsnit'     , element : 'p', attributes: { } },
 
-  if(dialogName === 'image') {
-    infoTab.remove('txtBorder');
-  }
-});
+        {
+            name : 'Liste',
+            element : 'ul',
+            attributes :
+            {
+                'class' : 'list-unstyled'
+            }
+        },
+        {
+            name : 'Liste - horisontal',
+            element : 'ul',
+            attributes :
+            {
+                'class' : 'list-inline'
+            }
+        },
+        {
+            name : 'Tabel',
+            element : 'table',
+            attributes :
+            {
+                'class' : 'table'
+            }
+        },
+        {
+            name : 'Tabel - stribet',
+            element : 'table',
+            attributes :
+            {
+                'class' : 'table table-striped'
+            }
+        },
+        {
+            name : 'Tabel - m ramme',
+            element : 'table',
+            attributes :
+            {
+                'class' : 'table table-bordered'
+            }
+        },
+        {
+            name : 'Tabel - mouse-over effekt',
+            element : 'table',
+            attributes :
+            {
+                'class' : 'table table-hover'
+            }
+        },
+        {
+            name : 'Tabel - lille margin',
+            element : 'table',
+            attributes :
+            {
+                'class' : 'table table-condensed'
+            }
+        },
+        {
+            name : 'Billede - runde hjørner',
+            element : 'img',
+            attributes :
+            {
+                'class' : 'img-rounded'
+            }
+        },
+        {
+            name : 'Billede - rundt',
+            element : 'img',
+            attributes :
+            {
+                'class' : 'img-circle'
+            }
+        },
+
+        {
+            name : 'Billede - flyder t venstre',
+            element : 'img',
+            attributes :
+            {
+                'class' : 'pull-left'
+            }
+        },
+        {
+            name : 'Billede - flyder t højre',
+            element : 'img',
+            attributes :
+            {
+                'class' : 'pull-right'
+            }
+        },
+
+    ]);
+}
