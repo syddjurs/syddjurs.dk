331250c79f25b91f8e3afa848a013edb8e3cf27c
juuliabellcom <juulia@bellcom.dk>
2017-05-08 16:13:52 +0300

    Menu fix

--- public_html/sites/all/themes/custom/site/dist/js/app.js
+++ public_html/sites/all/themes/custom/site/dist/js/app.js
@@ -4875,31 +4875,28 @@ var equalHeight = (function ($) {
 
       var $parent = $(this).parents('.megamenu'),
           $megamenu = $parent.find('.megamenu-panel');
-      var megamenu_height =$megamenu.height();
-      if ($parent.hasClass('open'))
-        $('#toplevelmenu').animate({'height' : default_zone_menu_height}, 'swing', function(){
-          $('.megamenu')
-            .not($parent)
-            .find('.megamenu-panel')
-            .hide();
-        });
-      else
-        $('#toplevelmenu').animate({'height' : default_zone_menu_height + megamenu_height});
+     // var megamenu_height =$megamenu.height();
+      if (!$parent.hasClass('open')){
+        //$('#toplevelmenu').animate({'height' : default_zone_menu_height + megamenu_height});
+        jQuery('.megamenu-panel').hide();
 
-      $parent.toggleClass('open');
-      $megamenu
-        .addClass('clearfix')
-        .toggle();
-    });
+        var $megamenu = $parent.find('.megamenu-panel');
+        $megamenu.show();
+        var megamenu_height = $parent.find('.megamenu-panel').height();
+        $megamenu.addClass('clearfix');
+        jQuery('#toplevelmenu').height(default_zone_menu_height + megamenu_height);
+        jQuery('.megamenu').not($parent).removeClass('open');
+        $parent.addClass('open');
+      }
+     });
 
     $('.js-megamenu-close').on('click', function(event){
       event.preventDefault();
       var $parent = $(this).closest('.megamenu');
       if($parent.hasClass('open')){
         $parent.removeClass('open');
-        $('#toplevelmenu').animate({'height' : default_zone_menu_height}, 'swing', function(){
+        $('#toplevelmenu').height(default_zone_menu_height);
         $('.megamenu-panel').hide();
-        });
       }
     });
   }
--- public_html/sites/all/themes/custom/site/src/js/init.js
+++ public_html/sites/all/themes/custom/site/src/js/init.js
@@ -26,7 +26,7 @@
   // Megamenu
   toggleMegamenu();
 
-function toggleMegamenu() {
+  function toggleMegamenu() {
     var $toggle = $('.js-megamenu-toggle');
     var default_zone_menu_height = jQuery('#toplevelmenu').outerHeight();
     $('.megamenu-panel').each(function() {
@@ -38,31 +38,28 @@ function toggleMegamenu() {
 
       var $parent = $(this).parents('.megamenu'),
           $megamenu = $parent.find('.megamenu-panel');
-      var megamenu_height =$megamenu.height();
-      if ($parent.hasClass('open'))
-        $('#toplevelmenu').animate({'height' : default_zone_menu_height}, 'swing', function(){
-          $('.megamenu')
-            .not($parent)
-            .find('.megamenu-panel')
-            .hide();
-        });
-      else
-        $('#toplevelmenu').animate({'height' : default_zone_menu_height + megamenu_height});
+     // var megamenu_height =$megamenu.height();
+      if (!$parent.hasClass('open')){
+        //$('#toplevelmenu').animate({'height' : default_zone_menu_height + megamenu_height});
+        jQuery('.megamenu-panel').hide();
 
-      $parent.toggleClass('open');
-      $megamenu
-        .addClass('clearfix')
-        .toggle();
-    });
+        var $megamenu = $parent.find('.megamenu-panel');
+        $megamenu.show();
+        var megamenu_height = $parent.find('.megamenu-panel').height();
+        $megamenu.addClass('clearfix');
+        jQuery('#toplevelmenu').height(default_zone_menu_height + megamenu_height);
+        jQuery('.megamenu').not($parent).removeClass('open');
+        $parent.addClass('open');
+      }
+     });
 
     $('.js-megamenu-close').on('click', function(event){
       event.preventDefault();
       var $parent = $(this).closest('.megamenu');
       if($parent.hasClass('open')){
         $parent.removeClass('open');
-        $('#toplevelmenu').animate({'height' : default_zone_menu_height}, 'swing', function(){
+        $('#toplevelmenu').height(default_zone_menu_height);
         $('.megamenu-panel').hide();
-        });
       }
     });
   }
