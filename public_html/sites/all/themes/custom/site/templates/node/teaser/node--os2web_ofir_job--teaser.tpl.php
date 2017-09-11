e41f673df5d83444885d8d10b687b93d60992d5a
Franz Skaaning <fs@bellcom.dk>
2017-09-11 10:26:37 +0200

    tpl files

--- public_html/sites/all/themes/custom/site/templates/node/teaser/node--os2web_ofir_job--teaser.tpl.php
+++ public_html/sites/all/themes/custom/site/templates/node/teaser/node--os2web_ofir_job--teaser.tpl.php
@@ -1,34 +1,24 @@
-<?php if ($view_mode == 'teaser'): ?>
+<?php
+  $content['field_os2web_ofir_job_period']['#label_display']='hidden';
+  if ($view_mode == 'teaser'): ?>
   <!-- node--os2web_ofir_job--teaser.tpl.php -->
-  <!-- Begin - teaser -->
-  <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> os2-equal-height-element clearfix"<?php print $attributes; ?>>
-
-    <!-- Begin - heading -->
-    <div class="os2-node-teaser-heading">
-      <div>
-      <strong><?php print render($content['field_os2web_ofir_category']); ?></strong>
-    </div>
-      <h3 class="os2-node-teaser-heading-title"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-    </div>
-
-    <!-- Begin - body -->
-    <div class="os2-node-teaser-body">
-      <!-- Begin - intro -->
+  <article role="document" id="node-<?php print $node->nid; ?>" class="row <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+    <div class="col-sm-9">
+      <header class="os2-node-teaser-heading-title" role="heading">
+        <a class="h3" href="<?php print $node_url; ?>">
+          <?php print $title; ?>
+        </a>
+      </header>
       <?php if (isset($content['field_os2web_ofir_job_text'])): ?>
-      <div class="os2-node-teaser-body-content">
-        <?php print render($content['field_os2web_ofir_job_text']); ?>
-      </div>
+        <main class="os2-node-teaser-body" role="main">
+          <div class="os2-node-teaser-body-content">
+            <?php print render($content['field_os2web_ofir_job_text']); ?>
+          </div>
+        </main>
       <?php endif; ?>
-      <!-- End - intro -->
-
-      <div>
-        <strong><?php print render($content['field_os2web_ofir_job_period']); ?></strong>
-      </div>
     </div>
-    <!-- End - body -->
-
+    <footer role="contentinfo" class="col-sm-3">
+      <?php print render($content['field_os2web_ofir_job_period']); ?>
+    </strong>
   </article>
-  <hr>
-  <!-- End - teaser -->
-
 <?php endif; ?>
